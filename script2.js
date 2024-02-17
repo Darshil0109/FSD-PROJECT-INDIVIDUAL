1fde700ed40fed1365e5529bfdb93e1322485280
Darshil0109 <122811740+Darshil0109@users.noreply.github.com>
2024-02-17 18:34:40 +0530

    Updated 17 Feb

--- script2.js
+++ script2.js
@@ -1,3 +1,3 @@
-function openImagePage(src,game_title) {
-    window.location.href = 'game_page.html?src='+src + '&game_title=' + game_title;
+function openImagePage(src,game_title,logo) {
+    window.location.href = 'game_page.html?src='+src + '&game_title=' + game_title +'&logo='+logo;
 }
